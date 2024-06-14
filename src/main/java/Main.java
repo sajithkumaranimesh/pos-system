a0b58213e491929de0c05b47dfe3904a898aa516
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-06-14 20:20:29 +0530

    employee database connet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/category-form.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/employee-form.fxml"))));
         stage.show();
     }
 }
