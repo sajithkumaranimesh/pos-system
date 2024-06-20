32b042288202e5cd3143f0b0bf1fe1b7464a4d8f
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-06-20 13:47:26 +0530

    transaction part one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/category-form.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/place-order-form.fxml"))));
         stage.show();
     }
 }
